86974c85fb2adb0aeb2a5325e8ee8fd180bf58fd
Ankur Anand <93547910+armanankur@users.noreply.github.com>
2024-06-04 23:13:39 +0530

    Update List.js

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+//import React, { useContext } from 'react';
 import Product from '../Product/Product';
 // import data from '../../constant'
 import './List.css';
